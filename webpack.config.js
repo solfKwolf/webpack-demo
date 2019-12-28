9e1ec71d9b21ae1a6846e0d5ab821962219e693c
solfKwolf <13536812252@163.com>
2019-12-28 19:23:31 +0800

    自定义插件

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,15 @@
+//  自定义插件
+function HelloWorldPlugin(options) {
+  // 使用 options 设置插件实例……
+}
+
+HelloWorldPlugin.prototype.apply = function(compiler) {
+  compiler.plugin('done', function() {
+    console.log('Hello World!');
+  });
+};
+
+
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
@@ -9,4 +21,7 @@ module.exports = {
     contentBase: './public',
     publicPath: '/dist',  // 这个就不需要了,自动帮你引入好像 ./dist 跟 /dist 有很大区别
   },
+  plugins: [
+    new HelloWorldPlugin({options: true})
+  ]
 }
